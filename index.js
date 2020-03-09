653008134934951b26352ee1b1936902a45fe807
unix <unix.bio@gmail.com>
2020-03-09 17:24:12 +0800

    perf: reduce node traversal

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const visit = require('unist-util-visit')
 const chinesePunctuationReg = /([\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b])\n/g
 
 module.exports = () => {
-  const visitor = (node) => {
-    visit(node, 'text', textNode => {
-      if (chinesePunctuationReg.test(textNode.value)) {
-        textNode.value = textNode.value.replace(chinesePunctuationReg, '$1')
-      }
-    })
+  const visitor = (textNode) => {
+    if (chinesePunctuationReg.test(textNode.value)) {
+      textNode.value = textNode.value.replace(chinesePunctuationReg, '$1')
+    }
   }
   
   return tree => {
-    visit(tree, 'element', visitor)
+    visit(tree, 'text', visitor)
   }
 }
